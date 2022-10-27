6c867bfe0c06a5804b315d9bef6b25412707e869
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2022-10-27 09:20:50 +0200

    Enable GPG SSH agent

--- .config/zsh/zenv.zsh
+++ .config/zsh/zenv.zsh
@@ -44,3 +44,11 @@ export NVM_LAZY_LOAD_EXTRA_COMMANDS=('lvim' 'nvim' 'emacs')
 if [[ -f "$HOME/.config/zsh/special-${HOSTNAME}.zsh" ]]; then
   source "$HOME/.config/zsh/special-${HOSTNAME}.zsh"
 fi
+
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+
+export GPG_TTY=$(tty)
+gpg-connect-agent updatestartuptty /bye >/dev/null
